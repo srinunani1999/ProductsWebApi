0674642ab416cfaa6b85432a6f3aa5028fbf940c
srinu <kodatihemasrinivas@gmail.com>
2020-07-11 12:31:14 +0530

    Add project files.

---
+++ ProductsWebApi/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductsWebApi.Models;
+
+namespace ProductsWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private ProductsRepository productrepsitory;
+
+        // GET: api/<ProductController>
+        public ProductController(ProductsRepository productsRepository)
+        {
+            this.productrepsitory = productsRepository;
+        }
+        public IActionResult Get()
+        {
+            return Ok(productrepsitory.GetAll());
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = productrepsitory.Get(id);
+            if (product == null)
+            {
+                return StatusCode(500);
+
+            }
+            return Ok(product);
+
+        }
+    }
+}
---
+++ ProductsWebApi/Models/ProductsRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsWebApi.Models
+{
+    public class ProductsRepository
+    {
+        static List<Product> products = new List<Product>();
+        static ProductsRepository()
+        {
+            products.Add(new Product { Id = 101, Name = "Asus i5", Description = "8gb Ram", Price = 50000 }
+                );
+            products.Add(new Product { Id = 102, Name = "MSI i5", Description = "8gb Ram", Price = 60000 });
+
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return products;
+        }
+        public Product Get(int id)
+        {
+            return products.Find(p => p.Id == id);
+        }
+    }
+}
